7ce3b43219211317b9d6247b3ec53a2d2d2b805d
ashkotian84 <ash.karkera@gmail.com>
2023-03-06 16:35:30 -0800

    hiii

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+public static void main(String[] args) {
+System.out.println("Welcome to Git!");
+System.out.println("yang - This line was added by Dr. Yang.");
+// Add a new line with your information
+System.out.println("SacStateID - This line was added by First name name.");
+System.out.println("SacStateID - This line was added by First name Lasname.");
+System.out.println("SacStateID - This line was added by First name Lastname.");
+System.out.println("SacStateID - This line was added by First name Lastname.");
+System.out.println("SacStateID - This line was added by First name Lastname.");
+System.out.println("SacStateID - This line was added by First name Lastname.");
+System.out.println("SacStateID - This line was added by First name Lastname.");
+System.out.println("SacStateID - This line was added by First name Lastname.");
+System.out.println("SacStateID - This line was added by First name Lastname.");
+}
+}
